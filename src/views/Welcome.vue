997168f5073d43f854c2f767167eeab60a01e8ad
Erickpe8 <ericksperezc@gmail.com>
2025-09-01 20:42:05 -0500

    Actualizar el componente Welcome: mejorar la estructura y el renderizado condicional de equipos y tecnologías.

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -2,21 +2,25 @@
   <div class="p-0 m-0 min-h-screen">
     <Header/> 
     <div class="flex-grow">
-       <Hero/>
-       <!-- seccion de los equipos -->
-       <TeamTabs/>
-       <TeamTextSection/>
-       <TeamLaravelVue v-if="teamStore.isTeamActive('team1')" />
-       <TeamNetAngular v-if="teamStore.isTeamActive('team2')" />
-       
-       <!-- seccion de las tecnologias -->
-       <TechTabs/>
-       <TechTextSection/>
-      
-      <!-- Renderizado condicional basado en el tab de tecnología activo -->
-      <TechLaravelVue v-if="teamStore.isTechActive('tech1')" />
-      <TechNetAngular v-if="teamStore.isTechActive('tech2')" />
+      <Hero/>
+
+      <!-- seccion de los equipos -->
+      <TeamTabs/>
+      <TeamTextSection/>
+
+      <!-- team1 = Ing. Manuel (Externos) -> .NET/Angular -->
+      <TeamNetAngular v-if="teamStore.isTeamActive('team1')" />
+      <TeamLaravelVue v-else /> <!-- team2 = Ing. Galindo (Internos) -> Laravel/Vue -->
+
+      <!-- seccion de las tecnologias -->
+      <TechTabs/>
+      <TechTextSection/>
+
+      <!-- tech1 = .NET/Angular ; tech2 = Laravel/Vue -->
+      <TechNetAngular v-if="teamStore.isTechActive('tech1')" />
+      <TechLaravelVue v-else />
     </div>
+
     <Footer/>
     <Dev/>
   </div>
@@ -30,23 +34,20 @@ import Hero from '@/components/Hero.vue';
 import TeamTabs from '@/components/Teams/TeamTabs.vue';
 import TeamTextSection from '@/components/Teams/TeamTextSection.vue';
 import TeamLaravelVue from '@/components/Teams/TeamLaravelVue.vue';
-import TeamNetAngular from '@/components/Teams/TeamNetAngular.vue'
+import TeamNetAngular from '@/components/Teams/TeamNetAngular.vue';
 
 // Tech components
 import TechTabs from '@/components/Technologies/TechTabs.vue';
 import TechTextSection from '@/components/Technologies/TechTextSection.vue';
 import TechLaravelVue from '@/components/Technologies/TechLaravelVue.vue';
-import TechNetAngular from '@/components/Technologies/TechNetAngular.vue'
+import TechNetAngular from '@/components/Technologies/TechNetAngular.vue';
 
 import Footer from '@/components/Footer.vue';
 import Dev from '@/components/ModalDev.vue';
 
 // Pinia store
-import { useTeamStore } from '@/stores/teamStore'
-
-const teamStore = useTeamStore()
+import { useTeamStore } from '@/stores/teamStore';
+const teamStore = useTeamStore();
 </script>
 
-<style lang="css" scoped>
-
-</style>
+<style scoped></style>
