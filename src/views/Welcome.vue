55a2ba9691adef89f722fd7650644db9110ef5da
Erickpe8 <ericksperezc@gmail.com>
2025-09-03 13:49:29 -0500

    implementar patrón Strategy (renderizado dinámico) en Team y Tech

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,25 +1,20 @@
 <template>
   <div class="p-0 m-0 min-h-screen">
-    <Header/> 
+    <Header/>
     <div class="flex-grow">
       <Hero/>
 
-      <!-- seccion de los equipos -->
+      <!-- sección de los equipos -->
       <TeamTabs/>
       <TeamTextSection/>
+      <!-- Strategy + dynamic component -->
+      <component :is="CurrentTeamComp" />
 
-      <!-- team1 = Ing. Manuel (Internos) -> Laravel/Vue -->
-      <TeamLaravelVue v-if="teamStore.isTeamActive('team1')" />
-      <!-- team2 = Ing. Galindo (Externos) -> .NET/Angular -->
-      <TeamNetAngular v-else />
-
-      <!-- seccion de las tecnologias -->
+      <!-- sección de las tecnologías -->
       <TechTabs/>
       <TechTextSection/>
-
-      <!-- tech1 = Laravel/Vue ; tech2 = .NET/Angular -->
-      <TechLaravelVue v-if="teamStore.isTechActive('tech1')" />
-      <TechNetAngular v-else />
+      <!-- Strategy + dynamic component -->
+      <component :is="CurrentTechComp" />
     </div>
 
     <Footer/>
@@ -28,27 +23,35 @@
 </template>
 
 <script setup>
-import Header from '@/components/Header.vue'; 
-import Hero from '@/components/Hero.vue'; 
+import Header from '@/components/Header.vue'
+import Hero from '@/components/Hero.vue'
 
 // Team components
-import TeamTabs from '@/components/Teams/TeamTabs.vue';
-import TeamTextSection from '@/components/Teams/TeamTextSection.vue';
-import TeamLaravelVue from '@/components/Teams/TeamLaravelVue.vue';
-import TeamNetAngular from '@/components/Teams/TeamNetAngular.vue';
+import TeamTabs from '@/components/Teams/TeamTabs.vue'
+import TeamTextSection from '@/components/Teams/TeamTextSection.vue'
+import TeamLaravelVue from '@/components/Teams/TeamLaravelVue.vue'
+import TeamNetAngular from '@/components/Teams/TeamNetAngular.vue'
 
 // Tech components
-import TechTabs from '@/components/Technologies/TechTabs.vue';
-import TechTextSection from '@/components/Technologies/TechTextSection.vue';
-import TechLaravelVue from '@/components/Technologies/TechLaravelVue.vue';
-import TechNetAngular from '@/components/Technologies/TechNetAngular.vue';
+import TechTabs from '@/components/Technologies/TechTabs.vue'
+import TechTextSection from '@/components/Technologies/TechTextSection.vue'
+import TechLaravelVue from '@/components/Technologies/TechLaravelVue.vue'
+import TechNetAngular from '@/components/Technologies/TechNetAngular.vue'
 
-import Footer from '@/components/Footer.vue';
-import Dev from '@/components/ModalDev.vue';
+import Footer from '@/components/Footer.vue'
+import Dev from '@/components/ModalDev.vue'
 
-// Pinia store
-import { useTeamStore } from '@/stores/teamStore';
-const teamStore = useTeamStore();
+import { computed } from 'vue'
+import { useTeamStore } from '@/stores/teamStore'
+const teamStore = useTeamStore()
+
+const CurrentTeamComp = computed(() => (
+  teamStore.isTeamActive('team1') ? TeamLaravelVue : TeamNetAngular
+))
+
+const CurrentTechComp = computed(() => (
+  teamStore.isTechActive('tech1') ? TechLaravelVue : TechNetAngular
+))
 </script>
 
 <style scoped></style>
